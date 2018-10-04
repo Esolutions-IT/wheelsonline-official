9af530dddf4f22c9aabc728d40bc31160b1b1efd
Gertjan Egas <info@esolutions-it.nl>
2018-10-04 11:43:18 +0200

    Profiel picture insert

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,6 +66,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'profile_picture' => './resources/assets/images/profile_picture/profiel.png'
         ]);
     }
 }
