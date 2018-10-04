bee1f3d147f4c7cdd0e055b846fd8dfc8aed5480
Gertjan Egas <info@esolutions-it.nl>
2018-10-04 12:47:47 +0200

    Favicon Laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,6 +19,8 @@
     <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+    <link rel="shortcut icon" href="{{{ asset('public/images/wheelsonline.png') }}}">
     <style>
         html, body {
             background-image: url(./resources/assets/images/bmw-back.jpg);
