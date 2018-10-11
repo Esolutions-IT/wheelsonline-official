870b27dc02b80bfc1f5eb6d1ad19326817053b0e
ArjanRijkeboer <43805974+ArjanRijkeboer@users.noreply.github.com>
2018-10-11 13:50:16 +0200

    Update config/database.php

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'sql7.freesqldatabase.com'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'sql7259767'),
-            'username' => env('DB_USERNAME', 'sql7259767'),
-            'password' => env('DB_PASSWORD', '7WTbTxG8Jl'),
+            'database' => env('DB_DATABASE', 'wheelsonline'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
